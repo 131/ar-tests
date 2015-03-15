fce9c24572838df3c5f21d8aaef9ad09f0e74d75
François Leurent <131.php@cloudyks.org>
2015-03-15 20:46:41 +0100

    Proper use of event dispatch

--- html_moving_rect/anim.js
+++ html_moving_rect/anim.js
@@ -1,14 +1,18 @@
-$(window).on("AR_BEGIN AR_END", function(event){
+var end = function(event){
   console.log("AR", event.type);
-});
+};
+
+window.addEventListener("AR_BEGIN", end);
+window.addEventListener("AR_END", end);
 
 $(document).ready(function(){
   setTimeout(function(){
     $(window).trigger("AR_BEGIN");
+    window.dispatchEvent(new Event("AR_BEGIN"));
 
     $("#drawing").addClass("animate");
     setTimeout(function(){
-      $(window).trigger("AR_END");
+      window.dispatchEvent(new Event("AR_END"));
     }, 4000);
 
   }, 1000);
